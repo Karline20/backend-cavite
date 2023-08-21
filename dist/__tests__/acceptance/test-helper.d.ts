aa815c0ee671cb4f2744abd90a12d5d157919cd0
karle <karlen.legaspi20@gmail.com>
2023-08-22 01:29:55 +0800

    Initial commit

---
+++ dist/__tests__/acceptance/test-helper.d.ts
@@ -0,0 +1,7 @@
+import { BackendCaviteApplication } from '../..';
+import { Client } from '@loopback/testlab';
+export declare function setupApplication(): Promise<AppWithClient>;
+export interface AppWithClient {
+    app: BackendCaviteApplication;
+    client: Client;
+}
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, BackendCaviteApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<BackendCaviteApplication>;
